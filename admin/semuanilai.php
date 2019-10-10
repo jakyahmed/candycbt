cd754f34d8919856f0cb69163dbc8af87d8ff995
ukemakassar <uke.makassar@gmail.com>
2019-10-10 12:43:38 +0800

    Update semuanilai.php

--- admin/semuanilai.php
+++ admin/semuanilai.php
@@ -1,5 +1,4 @@
-<?php
-if ($ac == '') { ?>
+<?php if ($ac == '') : ?>
 	<div class='row'>
 		<div class='col-md-12'>
 			<div class='box box-solid'>
@@ -30,95 +29,85 @@ if ($ac == '') { ?>
 			</div>
 		</div>
 	</div>
-<?php } elseif ($ac == 'lihat') { ?>
+<?php elseif ($ac == 'lihat') : ?>
 	<?php
 		$id_kelas = $_GET['idk'];
-		$mapel = mysql_fetch_array(mysql_query("SELECT * FROM mapel WHERE id_mapel='$id_mapel'")); ?>
-	<div class='row'>
-		<div class='col-md-12'>
-			<div class='box box-solid'>
-				<div class='box-header with-border bg-blue'>
-					<h3 class='box-title'><img src='../dist/img/svg/daftar_nilai.svg' width='30'> NILAI <?= $mapel['nama'] ?> <?= $id_kelas ?></h3>
-					<div class='box-tools pull-right btn-group'>
-						<a class='btn btn-sm btn-primary' href='report_excel_all.php?m=<?= $id_mapel ?>&k=<?= $id_kelas ?>&i=<?= $kode_ujian ?>'><i class='fa fa-file-excel-o'></i> Excel</a>
-						<a class='btn btn-sm btn-primary' href='?pg=nilai'><i class='fa fa-times'></i> Keluar</a>
-					</div>
-				</div><!-- /.box-header -->
-				<div class='box-body'>
-					<div class='table-responsive'>
-						<table id='example' class='table table-bordered table-striped'>
-							<thead>
-								<tr>
-									<th rowspan='3' width='5px'>#</th>
-									<th style='text-align:center' rowspan='3'>No Peserta</th>
-									<th style='text-align:center' rowspan='3'>Nama Peserta</th>
-									<th style='text-align:center' rowspan='3'>Kelas</th>
-								<?php
-									$mapelQ = mysql_query("SELECT * FROM mapel a inner join nilai b ON a.id_mapel=b.id_mapel group by b.id_ujian ");
-									while ($mapel = mysql_fetch_array($mapelQ)) {
-										echo "
-																<th style='text-align:center' colspan='3'>$mapel[nama]</th>
-																";
-									}
-									echo "
-															</tr>
-															";
-									$kode = mysql_query("SELECT * FROM mapel a inner join nilai b ON a.id_mapel=b.id_mapel group by b.id_ujian");
-									while ($mapel = mysql_fetch_array($kode)) {
-										echo "
-																<th style='text-align:center' colspan='3'>$mapel[kode_ujian]</th>";
-									}
-									echo "
-															</tr>
-															<tr>";
-									$mapelQ = mysql_query("SELECT * FROM mapel a inner join nilai b ON a.id_mapel=b.id_mapel group by b.id_ujian ");
-									while ($mapel = mysql_fetch_array($mapelQ)) {
-										echo "
-																<th style='text-align:center'>B</th>
-																<th style='text-align:center'>S</th>
-																<th style='text-align:center'>SKOR</th>";
-									}
-									echo "
-															</tr>
-															</thead>
-															<tbody>
-														";
-									$siswaQ = mysql_query("SELECT * FROM siswa where id_kelas='$id_kelas' ORDER BY nama ASC");
-									while ($siswa = mysql_fetch_array($siswaQ)) {
-										$no++;
-										$ket = '';
-										$esai = $lama = $jawaban = $skor = $total = '--';
-
-
-
-										echo "
-																<tr>
-																	<td>$no</td>
-																	<td align='center'>$siswa[no_peserta]</td>
-																	<td>$siswa[nama]</td>
-																	<td align='center'>$siswa[id_kelas]</td>";
-										$mapelQ = mysql_query("SELECT * FROM mapel a inner join nilai b ON a.id_mapel=b.id_mapel group by b.id_ujian ");
-										while ($mapel = mysql_fetch_array($mapelQ)) {
-											$nilaiQ = mysql_query("SELECT * FROM nilai WHERE id_ujian='$mapel[id_ujian]' AND id_siswa='$siswa[id_siswa]' and kode_ujian='$mapel[kode_ujian]'");
-											$nilaiC = mysql_num_rows($nilaiQ);
-											$nilai = mysql_fetch_array($nilaiQ);
-											echo "
-																	<td align='center'>$nilai[jml_benar]</td>
-																	<td align='center'>$nilai[jml_salah]</td>
-																	<td align='center'>$nilai[skor]</td>";
-										}
-										echo "
-																</tr>
-															";
-									}
-									echo "
-														</tbody>
-													</table>
-													
-													</div>
-												</div><!-- /.box-body -->
-											</div><!-- /.box -->
-										</div>
-									</div>
-								";
-								} //input esai
+		$mapel = mysql_fetch_array(mysql_query("SELECT * FROM mapel")); ?>
+		<div class='row'>
+			<div class='col-md-12'>
+				<div class='box box-solid'>
+					<div class='box-header with-border bg-blue'>
+						<h3 class='box-title'><img src='../dist/img/svg/daftar_nilai.svg' width='30'> REKAP NILAI KELAS <?= $id_kelas ?></h3>
+						<div class='box-tools pull-right btn-group'>
+							<!-- <a class='btn btn-sm btn-primary' href='report_excel_all.php?m=<?= $id_mapel ?>&k=<?= $id_kelas ?>&i=<?= $kode_ujian ?>'><i class='fa fa-file-excel-o'></i> Excel</a> -->
+							<a class='btn btn-sm btn-primary' href='report_excel_all.php?k=<?= $id_kelas ?>'><i class='fa fa-file-excel-o'></i> Excel</a>
+							<a class='btn btn-sm btn-primary' href='?pg=nilai'><i class='fa fa-times'></i> Keluar</a>
+						</div>
+					</div><!-- /.box-header -->
+					<div class='box-body'>
+						<div class='table-responsive'>
+							<table id='example' class='table table-bordered table-striped'>
+								<thead>
+									<tr>
+										<th rowspan='3' width='5px'>#</th>
+										<th style='text-align:center' rowspan='3'>No Peserta</th>
+										<th style='text-align:center' rowspan='3'>Nama Peserta</th>
+										<th style='text-align:center' rowspan='3'>Kelas</th>
+										<?php
+												$mapelQ = mysql_query("SELECT * FROM mapel a INNER JOIN nilai b ON a.id_mapel=b.id_mapel GROUP BY b.id_ujian ");
+												while ($mapel = mysql_fetch_array($mapelQ)) :
+													echo "<th style='text-align:center' colspan='3'>$mapel[nama]</th>";
+												endwhile;
+												?>
+									</tr>
+									<tr>
+										<?php
+												$kode = mysql_query("SELECT * FROM mapel a INNER JOIN nilai b ON a.id_mapel=b.id_mapel GROUP BY b.id_ujian");
+												while ($mapel = mysql_fetch_array($kode)) :
+													echo "<th style='text-align:center' colspan='3'>$mapel[kode_ujian]</th>";
+												endwhile;
+												?>
+									</tr>
+									<tr>
+										<?php $mapelQ = mysql_query("SELECT * FROM mapel a INNER JOIN nilai b ON a.id_mapel=b.id_mapel GROUP BY b.id_ujian "); ?>
+										<?php while ($mapel = mysql_fetch_array($mapelQ)) : ?>
+											<th style='text-align:center'>B</th>
+											<th style='text-align:center'>S</th>
+											<th style='text-align:center'>SKOR</th>
+										<?php endwhile; ?>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $siswaQ = mysql_query("SELECT * FROM siswa WHERE id_kelas='$id_kelas' ORDER BY nama ASC"); ?>
+									<?php while ($siswa = mysql_fetch_array($siswaQ)) : ?>
+										<?php
+													$no++;
+													$ket = '';
+													$esai = $lama = $jawaban = $skor = $total = '--';
+													?>
+										<tr>
+											<td><?= $no ?></td>
+											<td style="text-align:center"><?= $siswa['no_peserta'] ?></td>
+											<td><?= $siswa['nama'] ?></td>
+											<td style="text-align:center"><?= $siswa['id_kelas'] ?></td>
+											<?php $mapelQ = mysql_query("SELECT * FROM mapel a INNER JOIN nilai b ON a.id_mapel=b.id_mapel GROUP BY b.id_ujian "); ?>
+											<?php while ($mapel = mysql_fetch_array($mapelQ)) : ?>
+												<?php
+																$nilaiQ = mysql_query("SELECT * FROM nilai WHERE id_ujian='$mapel[id_ujian]' AND id_siswa='$siswa[id_siswa]' and kode_ujian='$mapel[kode_ujian]'");
+																$nilaiC = mysql_num_rows($nilaiQ);
+																$nilai = mysql_fetch_array($nilaiQ);
+																?>
+												<td style="text-align:center"><?= $nilai['jml_benar'] ?></td>
+												<td style="text-align:center"><?= $nilai['jml_salah'] ?></td>
+												<td style="text-align:center"><?= $nilai['skor'] ?></td>
+											<?php endwhile; ?>
+										</tr>
+									<?php endwhile; ?>
+								</tbody>
+							</table>
+						</div>
+					</div><!-- /.box-body -->
+				</div><!-- /.box -->
+			</div>
+		</div>
+<?php endif; ?>

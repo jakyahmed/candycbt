707ff9032a9b621ce4c41ee9aa138ec61e44a316
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:16 +0800

    Update ambiljawaban.php

--- admin/ambiljawaban.php
+++ admin/ambiljawaban.php
@@ -2,9 +2,9 @@
 require("../config/config.default.php");
 require("../config/config.function.php");
 $idu = $_POST['id'];
-$mapelQ = mysql_query("SELECT * from jawaban where id_ujian='$idu' ");
+$mapelQ = mysqli_query($koneksi, "SELECT * from jawaban WHERE id_ujian='$idu' ");
 
-while ($jawab = mysql_fetch_array($mapelQ)) {
-	$exec = mysql_query("insert into hasil_jawaban (id_siswa,id_mapel,id_soal,jawaban,jenis,esai,nilai_esai,ragu,id_ujian)values('$jawab[id_siswa]','$jawab[id_mapel]','$jawab[id_soal]','$jawab[jawaban]','$jawab[jenis]','$jawab[esai]','$jawab[nilai_esai]','$jawab[ragu]','$jawab[id_ujian]')");
+while ($jawab = mysqli_fetch_array($mapelQ)) {
+	$exec = mysqli_query($koneksi, "INSERT INTO hasil_jawaban (id_siswa,id_mapel,id_soal,jawaban,jenis,esai,nilai_esai,ragu,id_ujian)VALUES('$jawab[id_siswa]','$jawab[id_mapel]','$jawab[id_soal]','$jawab[jawaban]','$jawab[jenis]','$jawab[esai]','$jawab[nilai_esai]','$jawab[ragu]','$jawab[id_ujian]')");
 }
-$exec = mysql_query("delete from jawaban where id_ujian='$idu' ");
+$exec = mysqli_query($koneksi, "DELETE FROM jawaban WHERE id_ujian='$idu'");

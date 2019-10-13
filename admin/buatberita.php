49c1a31b1499248e64872425d8b0995156ac2899
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:23 +0800

    Update buatberita.php

--- admin/buatberita.php
+++ admin/buatberita.php
@@ -3,14 +3,14 @@
 require("../config/config.default.php");
 require("../config/config.function.php");
 
-$exec = mysql_query("TRUNCATE berita");
-$beritaQ = mysql_query("SELECT * FROM ujian");
-while ($berita = mysql_fetch_array($beritaQ)) {
-	$sesi = mysql_num_rows(mysql_query("SELECT * FROM siswa GROUP BY sesi"));
-	$ruangq = mysql_query("SELECT * FROM ruang");
-	while ($ruang = mysql_fetch_array($ruangq)) {
+$exec = mysqli_query($koneksi, "TRUNCATE berita");
+$beritaQ = mysqli_query($koneksi, "SELECT * FROM ujian");
+while ($berita = mysqli_fetch_array($beritaQ)) {
+	$sesi = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM siswa GROUP BY sesi"));
+	$ruangq = mysqli_query($koneksi, "SELECT * FROM ruang");
+	while ($ruang = mysqli_fetch_array($ruangq)) {
 		for ($i = 1; $i <= $sesi; $i++) {
-			$exec = mysql_query("INSERT INTO berita (id_mapel,sesi,ruang,jenis)VALUES('$berita[id_mapel]','$i','$ruang[kode_ruang]','$berita[kode_ujian]')");
+			$exec = mysqli_query($koneksi, "INSERT INTO berita (id_mapel,sesi,ruang,jenis)VALUES('$berita[id_mapel]','$i','$ruang[kode_ruang]','$berita[kode_ujian]')");
 		}
 	}
 }

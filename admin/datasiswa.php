637301c54ff4ff413746f49acca440bb8c6ca039
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:35 +0800

    Update datasiswa.php

--- admin/datasiswa.php
+++ admin/datasiswa.php
@@ -1,10 +1,10 @@
 <?php
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
 	include '../config/config.default.php';
-	$query = mysql_query("SELECT * FROM siswa ORDER BY nama ASC");
+	$query = mysqli_query($koneksi, "SELECT * FROM siswa ORDER BY nama ASC");
 	$jsonResult = '{"data" : [ ';
 	$i = 0;
-	while ($data = mysql_fetch_assoc($query)) {
+	while ($data = mysqli_fetch_assoc($query)) {
 		if ($i != 0) {
 			$jsonResult .= ',';
 		}

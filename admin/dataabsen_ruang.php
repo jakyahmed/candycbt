47983d9116d43374b106e8df24242b4771ca3e07
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:30 +0800

    Update dataabsen_ruang.php

--- admin/dataabsen_ruang.php
+++ admin/dataabsen_ruang.php
@@ -1,9 +1,9 @@
 <?php
 require("../config/config.default.php");
 $id_kelas = $_POST['id_kelas'];
-$sql = mysql_query("SELECT * FROM siswa WHERE id_kelas ='$id_kelas' GROUP BY ruang");
+$sql = mysqli_query($koneksi, "SELECT * FROM siswa WHERE id_kelas ='$id_kelas' GROUP BY ruang");
 echo "<option value=''>Pilih Ruang</option>";
-while ($ruang = mysql_fetch_array($sql)) {
+while ($ruang = mysqli_fetch_array($sql)) {
 	echo "<option value='$ruang[ruang]'>$ruang[ruang]</option>";
 }
 

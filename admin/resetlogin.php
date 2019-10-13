3eadab1cf1356fc9ec3d39c93fbb8ebaf132d630
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:14:09 +0800

    Update resetlogin.php

--- admin/resetlogin.php
+++ admin/resetlogin.php
@@ -3,7 +3,7 @@
 require("../config/config.default.php");
 require("../config/config.function.php");
 	$kode=$_POST['kode'];
-	$query = mysql_query('delete from login where id_log in ('.$kode.')');
+	$query = mysqli_query($koneksi, 'delete from login where id_log in ('.$kode.')');
 	if($query){
 		echo 1;
 	}

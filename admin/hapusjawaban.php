6e5248f284f577d6e42af5335c551c46bc1d8fcf
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:45 +0800

    Update hapusjawaban.php

--- admin/hapusjawaban.php
+++ admin/hapusjawaban.php
@@ -4,4 +4,4 @@ require("../config/config.default.php");
 require("../config/config.function.php");
 $kode = $_POST['id'];
 
-$exec = mysql_query("DELETE FROM hasil_jawaban WHERE id_ujian='$kode'");
+$exec = mysqli_query($koneksi, "DELETE FROM hasil_jawaban WHERE id_ujian='$kode'");

b2a0bdf51c12c236637dec3b810f59284a606eae
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:47 +0800

    Update hapusnilai.php

--- admin/hapusnilai.php
+++ admin/hapusnilai.php
@@ -4,4 +4,4 @@ require("../config/config.default.php");
 require("../config/config.function.php");
 $kode = $_POST['id'];
 
-$exec = mysql_query("DELETE FROM nilai WHERE id_ujian='$kode' and ujian_selesai<>''");
+$exec = mysqli_query($koneksi, "DELETE FROM nilai WHERE id_ujian='$kode' and ujian_selesai<>''");

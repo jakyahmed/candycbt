2ad1b043106281bcba80899ce62ddceba5f69122
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:48 +0800

    Update importsiswa.php

--- admin/importsiswa.php
+++ admin/importsiswa.php
@@ -16,7 +16,7 @@ if ($ext <> 'xls') {
 	$data = new Spreadsheet_Excel_Reader($temp);
 	$hasildata = $data->rowcount($sheet_index = 0);
 	$sukses = $gagal = 0;
-	$exec = mysql_query("TRUNCATE siswa");
+	$exec = mysqli_query($koneksi, "TRUNCATE siswa");
 	for ($i = 2; $i <= $hasildata; $i++) {
 		$id_siswa = $data->val($i, 1);
 		$nis = $data->val($i, 2);
@@ -43,37 +43,37 @@ if ($ext <> 'xls') {
 			$password = $data->val($i, 10);
 			$foto = $data->val($i, 11);
 		}
-		$qkelas = mysql_query("SELECT id_kelas FROM kelas WHERE id_kelas='$kelas'");
-		$cekkelas = mysql_num_rows($qkelas);
+		$qkelas = mysqli_query($koneksi, "SELECT id_kelas FROM kelas WHERE id_kelas='$kelas'");
+		$cekkelas = mysqli_num_rows($qkelas);
 		if (!$cekkelas <> 0) {
-			$exec = mysql_query("INSERT INTO kelas (id_kelas,level,nama)VALUES('$kelas','$level','$kelas')");
+			$exec = mysqli_query($koneksi, "INSERT INTO kelas (id_kelas,level,nama)VALUES('$kelas','$level','$kelas')");
 		}
 		if ($setting['jenjang'] == 'SMK') {
-			$qpk = mysql_query("SELECT id_pk FROM pk WHERE id_pk='$pk'");
-			$cekpk = mysql_num_rows($qpk);
+			$qpk = mysqli_query($koneksi, "SELECT id_pk FROM pk WHERE id_pk='$pk'");
+			$cekpk = mysqli_num_rows($qpk);
 			if (!$cekpk <> 0) {
-				$exec = mysql_query("INSERT INTO pk (id_pk,program_keahlian)VALUES('$pk','$pk')");
+				$exec = mysqli_query($koneksi, "INSERT INTO pk (id_pk,program_keahlian)VALUES('$pk','$pk')");
 			}
 		}
-		$qlevel = mysql_query("SELECT kode_level FROM level WHERE kode_level='$level'");
-		$ceklevel = mysql_num_rows($qlevel);
+		$qlevel = mysqli_query($koneksi, "SELECT kode_level FROM level WHERE kode_level='$level'");
+		$ceklevel = mysqli_num_rows($qlevel);
 		if (!$ceklevel <> 0) {
-			$exec = mysql_query("INSERT INTO level (kode_level,keterangan)VALUES('$level','$level')");
+			$exec = mysqli_query($koneksi, "INSERT INTO level (kode_level,keterangan)VALUES('$level','$level')");
 		}
-		$qruang = mysql_query("SELECT kode_ruang FROM ruang WHERE kode_ruang='$ruang'");
-		$cekruang = mysql_num_rows($qruang);
+		$qruang = mysqli_query($koneksi, "SELECT kode_ruang FROM ruang WHERE kode_ruang='$ruang'");
+		$cekruang = mysqli_num_rows($qruang);
 		if (!$cekruang <> 0) {
-			$exec = mysql_query("INSERT INTO ruang (kode_ruang,keterangan)VALUES('$ruang','$ruang')");
+			$exec = mysqli_query($koneksi, "INSERT INTO ruang (kode_ruang,keterangan)VALUES('$ruang','$ruang')");
 		}
-		$qsesi = mysql_query("SELECT kode_sesi FROM sesi WHERE kode_sesi='$sesi'");
-		$ceksesi = mysql_num_rows($qsesi);
+		$qsesi = mysqli_query($koneksi, "SELECT kode_sesi FROM sesi WHERE kode_sesi='$sesi'");
+		$ceksesi = mysqli_num_rows($qsesi);
 		if (!$ceksesi <> 0) {
-			$exec = mysql_query("INSERT INTO sesi (kode_sesi,nama_sesi)VALUES('$sesi','$sesi')");
+			$exec = mysqli_query($koneksi, "INSERT INTO sesi (kode_sesi,nama_sesi)VALUES('$sesi','$sesi')");
 		}
 		if ($setting['jenjang'] == 'SMK') {
-			$exec = mysql_query("INSERT INTO siswa (id_siswa,id_kelas,idpk,nis,no_peserta,nama,level,sesi,ruang,username,password,foto) VALUES ('$id_siswa','$kelas','$pk','$nis','$no_peserta','$nama','$level','$sesi','$ruang','$username','$password','$foto')");
+			$exec = mysqli_query($koneksi, "INSERT INTO siswa (id_siswa,id_kelas,idpk,nis,no_peserta,nama,level,sesi,ruang,username,password,foto) VALUES ('$id_siswa','$kelas','$pk','$nis','$no_peserta','$nama','$level','$sesi','$ruang','$username','$password','$foto')");
 		} else {
-			$exec = mysql_query("INSERT INTO siswa (id_siswa,id_kelas,nis,no_peserta,nama,level,sesi,ruang,username,password,foto) VALUES ('$id_siswa','$kelas','$nis','$no_peserta','$nama','$level','$sesi','$ruang','$username','$password','$foto')");
+			$exec = mysqli_query($koneksi, "INSERT INTO siswa (id_siswa,id_kelas,nis,no_peserta,nama,level,sesi,ruang,username,password,foto) VALUES ('$id_siswa','$kelas','$nis','$no_peserta','$nama','$level','$sesi','$ruang','$username','$password','$foto')");
 		}
 		($exec) ? $sukses++ : $gagal++;
 	}

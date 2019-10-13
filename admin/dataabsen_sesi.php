510223661908d499e926ddd823395a33ac607b52
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:32 +0800

    Update dataabsen_sesi.php

--- admin/dataabsen_sesi.php
+++ admin/dataabsen_sesi.php
@@ -1,8 +1,8 @@
 <?php
 require("../config/config.default.php");
 $id_kelas = $_POST['id_kelas'];
-$sql = mysql_query("SELECT * FROM siswa WHERE id_kelas ='$id_kelas' GROUP BY sesi");
+$sql = mysqli_query($koneksi, "SELECT * FROM siswa WHERE id_kelas ='$id_kelas' GROUP BY sesi");
 echo "<option value=''>Pilih Sesi</option>";
-while ($sesi = mysql_fetch_array($sql)) {
+while ($sesi = mysqli_fetch_array($sql)) {
 	echo "<option value='$sesi[sesi]'>$sesi[sesi]</option>";
 }

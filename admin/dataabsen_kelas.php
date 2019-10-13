a9b77d9efe8cf70171caaae836d38759167189d6
ukemakassar <uke.makassar@gmail.com>
2019-10-13 19:13:28 +0800

    Update dataabsen_kelas.php

--- admin/dataabsen_kelas.php
+++ admin/dataabsen_kelas.php
@@ -1,25 +1,25 @@
 <?php
 require("../config/config.default.php");
 $id_mapel = $_POST['mapel_id'];
-$sql = mysql_fetch_array(mysql_query("SELECT * FROM mapel WHERE id_mapel='$id_mapel'"));
+$sql = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM mapel WHERE id_mapel='$id_mapel'"));
 $dataArray = unserialize($sql['kelas']);
 
 if (count($dataArray) == 1) {
     if ($dataArray[0] == "semua") {
         $status = 0;
         if ($sql['level'] == "semua") {
-            $data = mysql_query("SELECT * FROM kelas");
+            $data = mysqli_query($koneksi, "SELECT * FROM kelas");
         } else {
             $tingkat = $sql['level'];
             $jurusan = $sql['idpk'];
             if ($sql['idpk'] == "semua") {
-                $data = mysql_query("SELECT * FROM kelas WHERE level='$tingkat'");
+                $data = mysqli_query($koneksi, "SELECT * FROM kelas WHERE level='$tingkat'");
             }else{
-                $data = mysql_query("SELECT * FROM kelas WHERE level='$tingkat' AND id_kelas LIKE '%$jurusan%'");
+                $data = mysqli_query($koneksi, "SELECT * FROM kelas WHERE level='$tingkat' AND id_kelas LIKE '%$jurusan%'");
             }
         }
         echo "<option value=''>Pilih Kelas</option>";
-        while ($kelas = mysql_fetch_array($data)) {
+        while ($kelas = mysqli_fetch_array($data)) {
             echo "<option value='$kelas[id_kelas]'>$kelas[nama]</option>";
         }
     } else {

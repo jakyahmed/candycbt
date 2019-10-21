bca0e2c355dfa8d9c8eb5be5c90b58a8623ccdb6
ukemakassar <uke.makassar@gmail.com>
2019-10-21 18:34:51 +0800

    Update config.default.php

--- config/config.default.php
+++ config/config.default.php
@@ -44,6 +44,7 @@ if ($koneksi) {
 		$query = mysqli_query($koneksi, "SELECT * FROM setting WHERE id_setting='1'");
 		if ($query) {
 			$setting = mysqli_fetch_array($query);
+			mysqli_set_charset($koneksi, 'utf8');
 			$sess = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM session WHERE id='1'"));
 			date_default_timezone_set($setting['waktu']);
 		}

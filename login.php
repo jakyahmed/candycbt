cd35a8ceed6151a08c498786b5e6af176f523856
Pajar Sidik N <pajarsidikn@gmail.com>
2019-11-12 19:21:31 +0700

    Update login.php

--- login.php
+++ login.php
@@ -13,32 +13,34 @@ if ($cekdb == false) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Login | <?php echo $setting['aplikasi']; ?></title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="dist/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="dist/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="dist/vendor/animate/animate.css">
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="dist/css/util.css">
 	<link rel="stylesheet" type="text/css" href="dist/css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel='stylesheet' href='<?php echo $homeurl; ?>/plugins/sweetalert2/dist/sweetalert2.min.css'>
 </head>
+
 <body>
-	
+
 	<div class="limiter">
-		<div class="container-login100" style="background:#bbe1f3;">
+		<div class="container-login100">
 			<div class="animated tada wrap-login100" style="padding-top:30px">
 				<form id="formlogin" action="ceklogin.php" class="login100-form validate-form">
-					
+
 					<span class="animated infinite pulse delay-5s login100-form-title p-b-40">
 						<img src="<?php echo $setting['logo']; ?>" style="max-height:100px" class="img-responsive" alt="Responsive image">
 					</span>
@@ -46,7 +48,7 @@ if ($cekdb == false) {
 						<?php echo $setting['sekolah']; ?>
 						<p><small>Support By <?= APLIKASI . " v" . VERSI . " r" . REVISI ?></small></p>
 					</span>
-					<div class="wrap-input100 validate-input" data-validate = "Enter Username" required>
+					<div class="wrap-input100 validate-input" data-validate="Enter Username" required>
 						<input class="input100" type="text" name="username">
 						<span class="focus-input100" data-placeholder="Username"></span>
 					</div>
@@ -68,23 +70,23 @@ if ($cekdb == false) {
 						</div>
 					</div>
 
-					
+
 				</form>
 			</div>
 		</div>
 	</div>
-	
+
 
 	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
+
+	<!--===============================================================================================-->
 	<script src="dist/vendor/jquery/jquery-3.2.1.min.js"></script>
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="dist/vendor/bootstrap/js/popper.js"></script>
 	<script src="dist/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-<script src='<?php echo $homeurl; ?>/plugins/sweetalert2/dist/sweetalert2.min.js'></script>
+	<!--===============================================================================================-->
+	<script src='<?php echo $homeurl; ?>/plugins/sweetalert2/dist/sweetalert2.min.js'></script>
 	<script src="dist/js/main.js"></script>
 	<script>
 		$(document).ready(function() {
@@ -147,4 +149,5 @@ if ($cekdb == false) {
 	</script>
 
 </body>
+
 </html>

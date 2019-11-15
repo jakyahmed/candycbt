8ebb3ce2fe992d67a86f8bfdd256571264dfe99c
ukemakassar <uke.makassar@gmail.com>
2019-11-15 18:52:35 +0800

    Update sinkronisasi.php

--- admin/sinkronisasi.php
+++ admin/sinkronisasi.php
@@ -1,190 +1,64 @@
-<?php
-
-$banksoalx = mysqli_num_rows(mysqli_query($koneksi, "select * from mapel where status='1'"));
-$soalx = mysqli_num_rows(mysqli_query($koneksi, 'select * from soal'));
-$siswax = mysqli_num_rows(mysqli_query($koneksi, "select * from siswa "));
-$ujianx = mysqli_num_rows(mysqli_query($koneksi, "select * from ujian "));
+<?php //004fb
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
 ?>
-
-<div class='row'>
-    <div class='col-md-12'>
-        <div class='box box-solid'>
-            <div class='box-header with-border'>
-                <h3 class='box-title'><i class='fa fa-download'></i> Tarik Data</h3>
-            </div><!-- /.box-header -->
-            <div class='box-body'>
-                <div class='box box-solid'>
-                    <div class='box-header with-border'>
-
-                        <h5 class='box-title'><i class='fa fa-feed'></i> CEK STATUS DATA </h5>
-
-                    </div>
-                    <div class='box-body'>
-                        <center><button class='btn btn-primary' id='btnstatus' data-toggle='modal' data-target='#cekstatus'><i class='fa fa-refresh'></i>&nbsp;Cek Status Data</button>
-                            <button class='btn btn-danger' id='btnreset' data-toggle='modal' data-target='#resetsinkron'><i class='fa fa-close'></i>&nbsp;Reset Sinkroniasi</button>
-                        </center>
-                    </div>
-                </div>
-                <div class='box box-solid'>
-                    <div class='box-header with-border'>
-
-                        <h5 class='box-title'><i class='fa fa-spinner fa-spin'></i> STATUS SINKRONISASI </h5>
-
-                    </div>
-                    <div class='box-body' id='resethasil'>
-
-                        <div id='progressbox'>
-                        </div>
-                        <div id='hasilsinkron'>
-                            <center><img id='loading-image2' src='ajax-loader.gif' style='display:none; width:50px;' />
-                                <center>
-                        </div>
-                        <table class='table table-striped' id='tablestatus'>
-                            <thead>
-                                <th>No</th>
-                                <th>Nama Data</th>
-                                <th>Jumlah</th>
-                                <th>Update Terakhir</th>
-                                <th>Status Sinkron</th>
-                                <th>Proses Sinkron</th>
-                            </thead>
-                            <tbody>
-                                <?php $no = 0;
-                                $dataq = mysqli_query($koneksi, "select * from sinkron");
-                                while ($data = mysqli_fetch_array($dataq)) {
-                                    if ($data['status_sinkron'] <> 0) {
-                                        $status = "<label class='label bg-green'>sudah sinkron</label>";
-                                        $button = "<button data-id='$data[nama_data]' class='sinkron btn bg-yellow'><i class='fa fa-recycle'></i> Ulang Sinkron</button>";
-                                    } else {
-                                        $status = "<label class='label bg-red'>belum sinkron</label>";
-                                        $button = "<button data-id='$data[nama_data]' class='sinkron btn bg-purple'><i class='fa fa-play'></i> Mulai Sinkron</button>";
-                                    }
-                                    if ($data['tanggal'] <> null) {
-                                        $update = "<button class='btn btn-flat bg-blue'>$data[tanggal]</button>";
-                                    } else {
-                                        $update = "<button class='btn btn-flat bg-blue'>BELUM ADA</button>";
-                                    }
-
-                                    $no++; ?>
-
-                                    <tr>
-                                        <td><?= $no ?></td>
-                                        <td><?= $data['nama_data'] ?></td>
-                                        <td><?= $data['jumlah'] ?></td>
-                                        <td><?= $update ?></td>
-                                        <td><?= $status ?></td>
-                                        <td><?= $button ?></td>
-                                    </tr>
-                                <?php    } ?>
-
-                            </tbody>
-
-                        </table>
-
-                    </div>
-                </div>
-            </div><!-- /.box-body -->
-        </div><!-- /.box -->
-    </div>
-</div>
-<div class='modal fade' id='cekstatus' style='display: none;'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-            <div class='modal-header bg-blue'>
-                <h3 class='modal-title'>Status Data Server</h3>
-            </div>
-            <div class='modal-body' id='bodystatus'>
-                <center><img id='loading-image' src='../dist/img/ajax-loader.gif' style='display:none; width:50px;' />
-                    <center>
-            </div>
-
-        </div>
-    </div>
-</div>
-<div class='modal fade' id='resetsinkron' style='display: none;'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-            <div class='modal-header bg-blue'>
-                <h3 class='modal-title'>Reset Sinkronisasi</h3>
-            </div>
-            <div class='modal-body' id='bodystatus'>
-                <center>Aksi ini akan menyebabkan semua data yang telah masuk terhapus, dan harus sinkronisasi ulang, apa anda yakin akan melanjutkan ??</center>
-            </div>
-            <div class='modal-footer'>
-
-                <center>
-                    <button class='btn btn-danger' name='reset' id='btnresetsinkron'><i class='fa fa-close'></i> IYA, Reset Sinkronisasi</button>
-                </center>
-
-            </div>
-        </div>
-    </div>
-</div>
-
-<script>
-    $('#btnstatus').click(function() {
-        $.ajax({
-            type: 'POST',
-            url: 'sinkronstatus.php',
-            beforeSend: function() {
-                $('#loading-image').show();
-            },
-            success: function(response) {
-                $('#bodystatus').html(response);
-
-                $('#mulaitarik').removeAttr('disabled');
-
-            },
-            complete: function() {
-                $('#loading-image').hide();
-            },
-        });
-
-    });
-    $('#btnresetsinkron').click(function() {
-        $.ajax({
-            type: 'POST',
-            url: 'sinkronreset.php',
-            beforeSend: function() {
-                $('.loader').css('display', 'block');
-            },
-            success: function(response) {
-                $('.loader').css('display', 'none');
-                $('#resetsinkron').modal('hide');
-                $('#tablestatus').load(location.href + ' #tablestatus');
-            }
-        });
-
-    });
-    $(document).on('click', '.sinkron', function() {
-        var dataid = $(this).data('id');
-        console.log(dataid);
-        swal({
-            title: 'Apakah kamu yakin?',
-            text: 'Berfungsi melakukan sinkronisasi dan mengulang sinkronisasi',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'YES!'
-        }).then((result) => {
-            if (result.value) {
-                $.ajax({
-                    type: 'POST',
-                    url: 'sinkron.php',
-                    data: 'data=' + dataid,
-                    beforeSend: function() {
-                        $(this).attr('disabled', 'disabled');
-                        $('.loader').css('display', 'block');
-
-                    },
-                    success: function(response) {
-                        $('.loader').css('display', 'none');
-                        $('#hasilsinkron').html(response);
-                        $('#tablestatus').load(location.href + ' #tablestatus');
-                    }
-                });
-            }
-        })
-    });
-</script>
+HR+cPuIOtle7I2FztbPBsMWbM4mu0ueKaaaDz+sS5YoD+oR6QYXbBbDjlvLEtvce1/uD5IfnmA54
+Dx8AqNFSzs+vyuNO2WC2JkzA1Y4WQ1C0ZhIiXO5TCAjAgNYag4o5ITtygmFJ10x1d6OL94zENeEa
+FXarrhCUT3YblOTyLKEoV+LwGTp8HJH/TgegPm1Z72AXLCNDQ4zjMGMw7BsZRC37yykR8+3+Uz0c
+H3chhNcBFH3oibFLagVjX1jOswOo7Ptkqkf8cyEeMNgr78fpPFMFRXnbfYovOmSK+QFA0+ZFIUuQ
+Jaq6IV/UsN+TBoHSbeVuQaIgOReBiMUqiawDBEbOM0RLr3RN+3Q3GnK/QTW5k3R7mlRmVFjaKkHV
+j/KQP3ETlSvdpV860xyp4UoJ9OvG8MPMMSYl5oMXAtif4loUeRE8ncYSZ0rRdrO8c4TaLqOXTthQ
+hqsFm2lDiP2ZCREqR3MoqqtOdD+iwPDtfTAwcDgpUuNbPZj5yHacdudgXQkCyKlbZMD2nnqF6b2S
+8qxTTSikfX8ZLReR2A0v2zc8U5q7hF/Sf292RHdXtgpuf9Z/uOcijbnC/corKfdqarwTbP3tZiAQ
+1BT9bv063obJ4Alzr8MV6e172Xvg+Hx/djWPTGIUEcnZ/rGh5HCZhzpjj/vaXO7uTXsFuje58SRi
+n3eeICARq8pcSvP9Isl/8m+/E3lZ/fjQmWwIVcB2N03IYUjqpYMWuMBgwvMlcHwiZYhEYXYTUe3K
+1AEwtq5uLe7kZlwDnnzpxikTs4QvDd7XR2yPuFojQxHlKhUcQ1WSpxH3UuL1gdEu7EN+PAGkXsNg
+pbN2hriVXVhujN3Bh4uX0rqZhBlAT9hZkPu51Np5pNIQvqbnMcL+IJje5xVOuMpZvOqEPSaa0cvD
+DvikRw+VzVwSmTxoqTm9eW/fhfa+MngpG8N0lQG6FhTkW/8hmdPujJhv9FQbGPD4iqfRlVnVCWel
+8ypS+ZN/erX2SQk4AGFYYOXq/yNuXpvXHbu/ycOb/i+KLhFeD9PlP/1YWr0Dyrn9/gDC9ec1rsmV
+ux8TyYl/s80hbUO6w+peZn59vffaAQ0KAthqNUdS0hTHgvdCQKFyOGyUJQTCynOrQH0+vbXpTuOW
+s+TFD2VEbp25ocCSB2LUspA7tvRssn9Zeg32BUc2p0/duaeadD504yxBQ2e+PP7dZ5gc3R1irDZg
+yAtumu8/gtPcwdMvcnBGVKvFy8wcfKXRtY5S2p2mmxihn6hY4t1Pwv81KrSVHQlYa25iL/c79cpO
+/Capr0+20hH+uLwz9e6C3ihy9QbXhRuxVoMo4tsM6IP1VwqsHKknD/f9gdO8Km+x5jjZTheqC3t6
+nl7U86xlJ/SvbJSU/xIYCD/200jr9+/LLOv2iqq/oVtjfsxg54tQTWxnpiqxkzkkfXoQXqRZaM9h
+zhU+riQJuAs5uzw+AqQ4cbzwn8ylLkHus0Pw1rs9SgZXSD5u//wubO8N5B5GQrlIabAYdM5MovpX
+lNgp02HpnuJPggQ7ljbr/td1pu3HUltZavD/mAm3P5hXDZxkgvyc7r5KBoHx011XVlM+iMBnxNKW
+Bio8AhE5PLP5hl2po7vkLVKbMyecqO2CkAwIAeWVvBGHGDORO3+vXixqyoZ0nIoHypenqX8zPt47
+ehDHtCZGP486/te0g10xxjD28y3atP6kmrtIm2zTXprqBXXtZy14kWpovDpp3YBei2NErO8pgXRm
+KafV/sD3YY49ojudmVhwPrhWLs6+9bwAmapttuWN8hmZ0+Igj+3cGrb6MB3Z+YPk7Bn2yTIlhfwR
+B3aWplPAG6a43AEFdtI2r0QzlM3bI2saCn+kazBuoDYebfnN+ribytskUh3MAW66ELUYUgTYD3ew
+UcXNPCy7UtTD6ln4oc9bFtkMEcwTDjUo55beThnG7nWpcKFcpulnELrqqbU3xXSxL28Ko+DRXjJP
+gZ+uPTzzL2kZ01no0obx7CTEN9tc2DczwHE0GdVBr2U2o8qSTYJ/CeMlS6vAx11pAMbBrKCfOTpq
+1fS4MYltkKQYmqXoUxLKRKi+TLa4TaY/rl/HmO2F79QBBL9EPIKl57mxjzVF/tWVwy5un1gWTrQr
+0ChyvZJNGkb1Vjg3Tr9AleY8ZmV681B4kGZ75R3OLnzItkU9ZddAS7A24O5h+00eqYIuEo3pR5GA
+Cdkn8lz3TlJMqG0d2pG74qBDsECHlyAr4yyRBEWJi91TKUip44chCUuOw2rfWIgBvsShxR4+h75N
+7m57g2Oaa545KcSvU3kn6d2R4IqUH7yYv+SPNeWQCH9d9oEDUSx2HGbpQ8qBTU/rzF8ab05VxMjH
+/ktZqelw6YMDLY3vye+KjSEy3rM7tQMma4yckDlfxejjAUmfOlNN9M7XyvO06jxzf+qCUp09Qx+A
+JIljMEB5qwR4CwzcNITUH1DaeKmS1+gbvcxxa2DyQyVwqedg+Pj2lmbuTUS3fypG4hpZVRMt09EL
+jvDVbZWKJo785coByapQoN6eA9uKa4tOSB1rLfB2lEGRViLLBiIV2koa4HWjpFsjgoTLGjFTUWOT
+igKWb9wgavCP8bxr86+5MZTWJmghpZ8nVMSq1MX1BqOgGEVanvwS+yE1Z62gJvDpc5vSPBJ8jYIe
+j9Nf4VR5oEkCOR4W1UklrRSsN5xJ6woiAhZzFr3aCmtlsdCBhlGlwsXK/ylUeA2nIjUFMZkB9+5i
+nZPM3yfdIG4L6mqJkqRLXFs/wsE80gUFETxisT+qY5/bfwjoUffxnlo7JyVLUIsGcQvzL99b24BF
+fUABD2e/3aag379aAsjtNOO3jMM1e+sx0RbIGHBiivUW78WME3YVumlrhMAquO94k+ptnFUJOX33
+0iK0S38z5NME4OLxkE38hjnopAkL4+/tzFHK3GexmJPUrc1DvnBiSC7gZ7MSOuNVtMsoKjOlmPC/
+NGOgB8vckpByJvkDrQYUlopCx7JOlGkRqR2uel9Dqo7hG0lEIRBImQ/59Vp+FRHtOaKAJHm4Rd28
+/wi834XYDlb6mtao76p/TMh7q6hattUeLKgqpJCjgZwOAuiCVqED3YUystFrlXqfTDrSTeCPXsK4
+AC+fpU295CSugVzi4sEsC3Jniiieay6A9p3gYhPWe3fIvFZHFgSVa3S+YhPS3wcYaAzbRTpHwf04
+RUcllWEF5F+k9lOIhivIHYLoCaT9h/msTGwUXATtQgdR1hY5VBwbzBvIpJ2wDAbth9TwSbEwb3NR
+qZxSBDGYLaQdEir2tgLPvNfwsd8kKQiLdgYPCEq/yUh5WSgLygscziIAH5AyVgQSB+1POm2ppRyE
+Q3+xgY2Ru0KjXh8QgRRLx86Wow4wOtrqfc4S5x2B4/4OKRTv7dmP8I+CLlzacgmft/VoEFn6WYbn
+mcgpd0lEy1yjemnPhTmdJGRZhHwaIJJJahQaDR0ei+TjLoamhAwohv0qmmCvC7f0pX2SOGUXeEhb
+wbiP6kXstgq4GOieJxt9ri96ZJK3eh9fosq0A2D32qWcsjv0/Bd73X2giGyNOPr4XWxAPKWjWl3i
+VYwAguFwteI94V39oDEa4vqooKUiqynmifdQ556kfUDh6dO0uXaI6v6zz8G8qDk6OQkfLPjIgJWB
+/S5xLwHoBD3GUhTNoQ+CxjjXNAVgdETlrva3lIiNiw1nheXORlE26C6AmRONVSC+gCuYZt4WLFgM
+ejX9O1r4lur16vBVC7i6/ungPLtexLhn952VrdmLJywcPOFUuAfNcEBKbtI1i0ZtJVfQeAg+D6io
+i3LgCUxrAlkpR50I5UiRiYVSao2A+dRCSzSYRaWiO7qieKxaqltRx8N5LI1p4lum+7p0RJT0zoMl
+HfLekVi+AZNnOLChwONNmOEcLU+OIKT94Q9QlO6IVMeXCuT5Ee650r+ayHPTvt+wxh0wfrljS+MB
+0ceKIelt4SgKsIDUmW0UCfCpd15MpNn7LBzEQGd7LfKaa/dEKsEYhhK0NGmfp9pOntu0kmLsQ+Uu
+6ZSfMCFkKrWeOtPDd2ufNt+de1qdOSr3L6dWX/NPeob3s+GHXRxDCMFUWY/5TmzvEdL7jstAgVYh
+WhLqJ7l9OUoAJe8upfALpGEm0KdgpkSgQydM3g/soNmVvlZWg3cHO0HdnQdFRnwI25zFYs5LhP0n
+BQXEO73T4DETLG9fj/7Drjdv3YALHwexSdNbOgEMnXNDTsJnP0VckZ6+XnldMuBVfF1STIfFxWxL
+BMSrno2DbBQcJdlUtXgAB7Ie2k8X4cKEkxIj/7PvwcCtzTEnoXYLcmKfKi0XWLdx8wz3Dq/K4xd8
+68PNUxpHPgrXpkA8+qIFuG0vhgpsLg8IcybcG/ovRoaK+yUnErR9uKrkgoZEqQy1Qkdhb8Fn9ZUY
+CXwShR/zdZ4ZRY6q2wfNbvKd5qVPbXaCqWs271trwFfy9D6m8RwOVOxlKev+0a858e9+J+3CnF9M
+yN1YStDi+oKSMZNWE1Rts/4f3qdoUB1CB595JWv45FJUPPVoR1PK3sCUybIbHjSHZ/WwJhIXX46e
+1tdSlVTA6Py=

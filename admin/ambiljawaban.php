4cc106133ff07d71ab7aaae9f37a2a5a38becda2
ukemakassar <uke.makassar@gmail.com>
2019-11-22 16:44:06 +0800

    Update ambiljawaban.php

--- admin/ambiljawaban.php
+++ admin/ambiljawaban.php
@@ -6,7 +6,8 @@ if (isset($_POST['id'])) {
 	$mapelQ = mysqli_query($koneksi, "SELECT * from jawaban WHERE id_ujian='$idu' ");
 
 	while ($jawab = mysqli_fetch_array($mapelQ)) {
-		$exec = mysqli_query($koneksi, "INSERT INTO hasil_jawaban (id_siswa,id_mapel,id_soal,jawaban,jenis,esai,nilai_esai,ragu,id_ujian,jawabx)VALUES('$jawab[id_siswa]','$jawab[id_mapel]','$jawab[id_soal]','$jawab[jawaban]','$jawab[jenis]','$jawab[esai]','$jawab[nilai_esai]','$jawab[ragu]','$jawab[id_ujian]','$jawab[jawabx]')");
+		$jawab_essai = addslashes($jawab['esai']);
+		$exec = mysqli_query($koneksi, "INSERT INTO hasil_jawaban (id_siswa,id_mapel,id_soal,jawaban,jenis,esai,nilai_esai,ragu,id_ujian,jawabx)VALUES('$jawab[id_siswa]','$jawab[id_mapel]','$jawab[id_soal]','$jawab[jawaban]','$jawab[jenis]','$jawab_essai','$jawab[nilai_esai]','$jawab[ragu]','$jawab[id_ujian]','$jawab[jawabx]')");
 	}
 	$exec = mysqli_query($koneksi, "DELETE FROM jawaban WHERE id_ujian='$idu'");
 } else {

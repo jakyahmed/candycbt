df8e3ac02d1d767726c9ab450c65d094f7aa30db
Pajar Sidik N <pajarsidikn@gmail.com>
2019-11-24 05:57:36 +0700

    Update buatberita.php

--- admin/buatberita.php
+++ admin/buatberita.php
@@ -4,7 +4,7 @@ require("../config/config.default.php");
 require("../config/config.function.php");
 
 $exec = mysqli_query($koneksi, "TRUNCATE berita");
-$beritaQ = mysqli_query($koneksi, "SELECT * FROM ujian group by level");
+$beritaQ = mysqli_query($koneksi, "SELECT * FROM ujian");
 while ($berita = mysqli_fetch_array($beritaQ)) {
 	$sesi = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM ujian GROUP BY sesi"));
 	$ruangq = mysqli_query($koneksi, "SELECT * FROM ruang");

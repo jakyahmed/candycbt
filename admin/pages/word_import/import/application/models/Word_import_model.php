2c3f17d430ed40b5d12cc2aecd097251def7d948
bob <bob@marley.com>
2020-03-11 13:38:27 +0700

    lowercase kunci support, aturan waktu selesai

--- admin/pages/word_import/import/application/models/Word_import_model.php
+++ admin/pages/word_import/import/application/models/Word_import_model.php
@@ -1,162 +1,169 @@
 <?php
-Class word_import_model extends CI_Model
+class word_import_model extends CI_Model
 {
 
-    function import_ques($question){
+    function import_ques($question)
+    {
         //echo "<pre>"; print_r($question);exit;
 
-        $questioncid=$this->input->post('cid');
-        $questiondid=$this->input->post('lid');
-        $idmapel=$this->input->post('id_mapel');
+        $questioncid = $this->input->post('cid');
+        $questiondid = $this->input->post('lid');
+        $idmapel = $this->input->post('id_mapel');
         $no = 0;
-        foreach($question as $key => $singlequestion){
+        foreach ($question as $key => $singlequestion) {
             //$ques_type=
 
             //echo $ques_type;
 
-            if($key != 0){
+            if ($key != 0) {
                 //echo "<pre>";print_r($singlequestion);
-                $question= str_replace('"','&#34;',$singlequestion['question']);
-                $question= str_replace("`",'&#39;',$question);
-                $question= str_replace("‘",'&#39;',$question);
-                $question= str_replace("’",'&#39;',$question);
-                $question= str_replace("â€œ",'&#34;',$question);
-                $question= str_replace("â€˜",'&#39;',$question);
-                $question= str_replace("â€™",'&#39;',$question);
-                $question= str_replace("â€",'&#34;',$question);
-                $question= str_replace("'","&#39;",$question);
-                $question= str_replace("&amp;lt;br&amp;gt;","<br>",$question);
-                $question= str_replace("\\n","<br>",$question);
+                $question = str_replace('"', '&#34;', $singlequestion['question']);
+                $question = str_replace("`", '&#39;', $question);
+                $question = str_replace("‘", '&#39;', $question);
+                $question = str_replace("’", '&#39;', $question);
+                $question = str_replace("â€œ", '&#34;', $question);
+                $question = str_replace("â€˜", '&#39;', $question);
+                $question = str_replace("â€™", '&#39;', $question);
+                $question = str_replace("â€", '&#34;', $question);
+                $question = str_replace("'", "&#39;", $question);
+                $question = str_replace("&amp;lt;br&amp;gt;", "<br>", $question);
+                $question = str_replace("&amp;lt;BR&amp;gt;", "<br>", $question);
+                $question = str_replace("\\n", "<br>", $question);
                 //print_r($question);exit;
 
                 //print_r($singlequestion['description']);
-                $description= str_replace('"','&#34;',$singlequestion['description']);
-                $description= str_replace("`",'&#39;',$description);
-                $description= str_replace("‘",'&#39;',$description);
-                $description= str_replace("’",'&#39;',$description);
-                $description= str_replace("â€œ",'&#34;',$description);
-                $description= str_replace("â€˜",'&#39;',$description);
+                $description = str_replace('"', '&#34;', $singlequestion['description']);
+                $description = str_replace("`", '&#39;', $description);
+                $description = str_replace("‘", '&#39;', $description);
+                $description = str_replace("’", '&#39;', $description);
+                $description = str_replace("â€œ", '&#34;', $description);
+                $description = str_replace("â€˜", '&#39;', $description);
                 //print_r($descirption);exit;
 
 
 
-                $description= str_replace("â€™",'&#39;',$description);
-                $description= str_replace("â€",'&#34;',$description);
-                $description= str_replace("'","&#39;",$description);
-                $description= str_replace("\n","<br>",$description);
+                $description = str_replace("â€™", '&#39;', $description);
+                $description = str_replace("â€", '&#34;', $description);
+                $description = str_replace("'", "&#39;", $description);
+                $description = str_replace("\n", "<br>", $description);
 
-
-
-                $option_count=count($singlequestion['option']);
-                $ques_type="1";
-                if($option_count!="0"){
-                    if($singlequestion['correct']!=""){
-                        $ques_type="1";//tipe soal dengan satu kunci jawaban
-                    }else{
+        
+                $option_count = count($singlequestion['option']);
+                $ques_type = "1";
+                if ($option_count != "0") { //ada pilihan ganda
+                    if ($singlequestion['correct'] != "") { //ada kunci
+                        $ques_type = "1"; //soal pil ganda
+                    } else {
                         //tidak ada kunci jawaban
-                        echo "<pre> Ada soal yang tidak memiliki kunci jawaban, perbaiki.";exit;
+                        echo "Ada soal yang tidak memiliki kunci jawaban, perbaiki.<br>";
+                        echo "Nomor soal :".($no+1)."<br>";
+                        echo  $singlequestion['question'];
+                        exit;
                     }
-                }else{
-                    //tidak ada pilihan ganda (esai)
-                    $ques_type="2";
+                } else { //tidak ada pilihan ganda
+                    if ($singlequestion['correct'] != "") { //jika ada kunci
+                        $ques_type = "1"; // soal pil ganda
+                    } else { //tidak ada kunci
+                        $ques_type = "2"; //soal esai
+                    }
                 }
 
-                $kunci=trim($singlequestion['correct']);
-                $corect_position=array(
-                        'A' => '0',
-                        'B' => '1',
-                        'C' => '2',
-                        'D' => '3',
-                        'E' => '4'
-                        );
-                $no=$no+1;
+                $kunci = strtoupper(trim($singlequestion['correct']));
+                $corect_position = array(
+                    'A' => '0',
+                    'B' => '1',
+                    'C' => '2',
+                    'D' => '3',
+                    'E' => '4',
+                    'a' => '0',
+                    'b' => '1',
+                    'c' => '2',
+                    'd' => '3',
+                    'e' => '4'
+                );
+                $no = $no + 1;
                 $insert_data = array(
-                'nomor' => $no,
-                'id_mapel' => $idmapel,
-                'jenis' => $ques_type,
-                'soal' =>$question,
-                'file' =>$description,
-                'jawaban' => $kunci,
+                    'nomor' => $no,
+                    'id_mapel' => $idmapel,
+                    'jenis' => $ques_type,
+                    'soal' => $question,
+                    'file' => $description,
+                    'jawaban' => $kunci,
                 );
 
 
                 //echo "<pre>";print_r($insert_data);exit;
                 // echo "<pre>";print_r($this->db);exit;
-                if($this->db->insert('soal',$insert_data)){
-                    $qid=$this->db->insert_id();
+                if ($this->db->insert('soal', $insert_data)) {
+                    $qid = $this->db->insert_id();
 
-                    if($ques_type=="1"){
-                        $update_data=array();
-                        foreach($singlequestion['option'] as $key=>$value){
-                            if(strpos($value,'FileO:')!==false){//jika pilihan berupa gambar
-                                $filename=substr($value,strpos($value,'FileO:')+6);
+                    if ($ques_type == "1") {
+                        if (!empty($singlequestion['option'])) {
+                            $update_data = array();
+                            foreach ($singlequestion['option'] as $key => $value) {
+                                $value=htmlspecialchars_decode($value);
+                                if (strpos($value, 'FileO:') !== false) { //jika pilihan berupa gambar
+                                    $filename = substr($value, strpos($value, 'FileO:') + 6);
 
-                                switch($key){
-                                    case 0:
-                                        $update_data["fileA"]=$filename;
-                                        break;
-                                    case 1:
-                                        $update_data["fileB"]=$filename;
-                                        break;
-                                    case 2:
-                                        $update_data["fileC"]=$filename;
-                                        break;
-                                    case 3:
-                                        $update_data["fileD"]=$filename;
-                                        break;
-                                    case 4:
-                                        $update_data["fileE"]=$filename;
-                                        break;
+                                    switch ($key) {
+                                        case 0:
+                                            $update_data["fileA"] = $filename;
+                                            break;
+                                        case 1:
+                                            $update_data["fileB"] = $filename;
+                                            break;
+                                        case 2:
+                                            $update_data["fileC"] = $filename;
+                                            break;
+                                        case 3:
+                                            $update_data["fileD"] = $filename;
+                                            break;
+                                        case 4:
+                                            $update_data["fileE"] = $filename;
+                                            break;
+                                    }
+                                } else { //pilihan bukan gambar
+
+                                    switch ($key) {
+                                        case 0:
+                                            $update_data["pilA"] = $value;
+                                            break;
+                                        case 1:
+                                            $update_data["pilB"] = $value;
+                                            break;
+                                        case 2:
+                                            $update_data["pilC"] = $value;
+                                            break;
+                                        case 3:
+                                            $update_data["pilD"] = $value;
+                                            break;
+                                        case 4:
+                                            $update_data["pilE"] = $value;
+                                            break;
+                                    }
                                 }
+                            } //endfor option
 
-                            }else{//pilihan bukan gambar
-
-                                switch($key){
-                                    case 0:
-                                        $update_data["pilA"]=$value;
-                                        break;
-                                    case 1:
-                                        $update_data["pilB"]=$value;
-                                        break;
-                                    case 2:
-                                        $update_data["pilC"]=$value;
-                                        break;
-                                    case 3:
-                                        $update_data["pilD"]=$value;
-                                        break;
-                                    case 4:
-                                        $update_data["pilE"]=$value;
-                                        break;
-                                }
-
+                            //print_r($update_data);
+                            if (!$this->db->update("soal", $update_data, "id_soal=" . $qid)) {
+                                echo $this->db->error();
+                                exit;
                             }
-                        }//endfor option
-                        //print_r($update_data);
-                        if(!$this->db->update("soal",$update_data,"id_soal=".$qid)){
-                            echo $this->db->error();exit;
                         }
                     }
 
-                    if($ques_type=="2"){
-
+                    if ($ques_type == "2") {
                     }
-
                 }
                 //insert soal gagal
-                else{
-                    echo "<pre> Gagal mengimport soal.</pre>";exit;
+                else {
+                    echo "<pre> Gagal mengimport soal.</pre>";
+                    exit;
                 }
-
-            }//key==0
+            } //key==0
 
 
 
         }
-
     }
-
 }
-
-?>
-
-
